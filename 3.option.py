08f44e6ebb40b0b8d66b83c9eb5c2c89f39a74d1
ONRiches <113181603+ONRiches@users.noreply.github.com>
2022-09-12 13:23:02 +0100

    Task 3

--- 3.option.py
+++ 3.option.py
@@ -4,10 +4,10 @@ selection = int(input())
 count = 0
 while count == 0:
     if selection < 1:
-        print("Please enter a number between 1 - 3")
+        print("Enter a number between 1 - 3")
         selection = int(input())
     if selection > 3:
-        print("Please enter a number between 1 - 3")
+        print("Enter a number between 1 - 3")
         selection = int(input())
     if selection < 4:
         if selection > 0:
