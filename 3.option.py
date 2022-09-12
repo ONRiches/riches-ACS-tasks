857dd9b57b3036200c6c8f2836259ec084610d12
ONRiches <113181603+ONRiches@users.noreply.github.com>
2022-09-12 13:22:17 +0100

    Create 3.option.py

---
+++ 3.option.py
@@ -0,0 +1,15 @@
+
+print("Enter a number between 1 - 3")
+selection = int(input())
+count = 0
+while count == 0:
+    if selection < 1:
+        print("Please enter a number between 1 - 3")
+        selection = int(input())
+    if selection > 3:
+        print("Please enter a number between 1 - 3")
+        selection = int(input())
+    if selection < 4:
+        if selection > 0:
+            print("You have selected option", selection)
+            count = count + 1
