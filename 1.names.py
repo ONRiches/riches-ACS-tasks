ea57fa9803a6a92fe6502a44203e00a50fed2dff
ONRiches <113181603+ONRiches@users.noreply.github.com>
2022-09-12 12:07:13 +0100

    Names Task

---
+++ 1.names.py
@@ -0,0 +1,10 @@
+# get the user to enter their first name
+print("Enter your first name")
+firstname = input()
+
+# get the user to enter their second name
+print("Enter your surname")
+surname = input()
+
+# print out both stored names in the correct order
+print(firstname, surname)
