92a02477ab7e7af62408214833f2ff139f52c411
ONRiches <113181603+ONRiches@users.noreply.github.com>
2022-09-14 09:12:20 +0100

    Task 3 Update

--- 3.option.py
+++ 3.option.py
@@ -3,10 +3,10 @@ print("Enter a number between 1 - 3")
 # Read the input
 selection = int(input())
 
-count = 0
+notfound = False
 
 # Use a while statement to keep prompting the user to enter an input until it is in the correct range
-while count == 0:
+while not(notfound):
 
     # check if the number is below 1
     if selection < 1 or selection > 3:
@@ -14,7 +14,7 @@ while count == 0:
         selection = int(input())
     else:
         print("You have selected option", selection)
-        count = count + 1
+        notfound = True
     # end if
 
 #end while
