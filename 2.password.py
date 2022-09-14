44672493c4881ddd8cb2dfeb53199eb63969d10a
ONRiches <113181603+ONRiches@users.noreply.github.com>
2022-09-14 08:53:41 +0100

    Task 2 update

--- 2.password.py
+++ 2.password.py
@@ -3,11 +3,8 @@ print("Please enter your password")
 password = input()
 
 # if statement to check if the password is more than 6 characters long
-if len(password) > 6:
+if len(password) > 6 or len(password) < 1:
     print("Invalid password")
-# end if
-
-# if statement to check if the password is less than 7 characters long
-if len(password) < 7:
+else:
     print("Valid password")
 # end if
