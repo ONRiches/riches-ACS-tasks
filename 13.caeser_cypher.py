d8f6059802b994f006cd05cef6d7c393b38c993f
ONRiches <113181603+ONRiches@users.noreply.github.com>
2022-09-16 08:56:42 +0100

    Task 13

--- 13.caeser_cypher.py
+++ 13.caeser_cypher.py
@@ -1,14 +1,18 @@
+# prompt the user to enter a sentence
 print("Please input your sentence to put turned into cyphertext")
 text = list(input())
 
 for count in range (1, len(text) + 1):
     char = ord(text[count - 1])
+    if char > 64 and char < 91:
+        char = char + 32
+    # end if
 
     if char == 121:
         char = 97
     elif char == 122:
         char = 98
-    else:
+    elif char < 121 and char > 96:
         char = char + 2
     # end if
 
