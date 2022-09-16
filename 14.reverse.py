d85b84a84598226a8dac78e32781e30d79953db0
ONRiches <113181603+ONRiches@users.noreply.github.com>
2022-09-16 09:08:38 +0100

    Task 14

--- 14.reverse.py
+++ 14.reverse.py
@@ -1,7 +1,18 @@
+# prompt the user to enter a string
 print("Pleae enter a word to be reversed")
-text = list(input())
+# read the input and convert it to a list
+text2 = input()
+text = list(text2)
 
+# reverse the list
 text.reverse()
-text = ''.join(text)
 
-print(text)
+# convert the list back to a string
+text1 = ''.join(text)
+
+# print the string
+print(text1)
+
+if text1 == text2:
+    print("This word is a pallindrome")
+# end if
