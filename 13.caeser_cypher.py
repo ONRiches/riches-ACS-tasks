60959bed75bc4306d674254aad79356ec6552b3f
ONRiches <113181603+ONRiches@users.noreply.github.com>
2022-09-16 09:03:39 +0100

    Task 14

--- 13.caeser_cypher.py
+++ 13.caeser_cypher.py
@@ -1,13 +1,18 @@
 # prompt the user to enter a sentence
 print("Please input your sentence to put turned into cyphertext")
+
+# convert the input to a list
 text = list(input())
 
 for count in range (1, len(text) + 1):
+
+    # convert uppcase letters to lowercase letters
     char = ord(text[count - 1])
     if char > 64 and char < 91:
         char = char + 32
     # end if
 
+    # create special cases for y, z and ignore any character that isn't a lowercase letter
     if char == 121:
         char = 97
     elif char == 122:
@@ -17,7 +22,9 @@ for count in range (1, len(text) + 1):
     # end if
 
     text[count - 1] = chr(char)
+
 # end for
 
+# revert the list to a string
 newtext = ''.join(text)
 print(newtext)
---
+++ 14.reverse.py
@@ -0,0 +1,7 @@
+print("Pleae enter a word to be reversed")
+text = list(input())
+
+text.reverse()
+text = ''.join(text)
+
+print(text)
