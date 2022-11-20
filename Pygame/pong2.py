af83bae0dc7628651ec1aa70143abe5a73fed0a2
ONRiches <113181603+ONRiches@users.noreply.github.com>
2022-11-20 18:27:27 +0000

    score update

--- Pygame/pong2.py
+++ Pygame/pong2.py
@@ -10,6 +10,8 @@ padd_length = 15
 padd_width = 60
 xspeed = 1
 yspeed = 1
+score = 0
+
 # -- Colours
 BLACK = (0,0,0)
 WHITE = (255,255,255)
@@ -17,6 +19,7 @@ BLUE = (50,50,255)
 YELLOW = (255,255,0)
 # -- Initialise PyGame
 pygame.init()
+font = pygame.font.Font('freesansbold.ttf', 32)
 # -- Blank Screen
 size = (640,480)
 screen = pygame.display.set_mode(size)
@@ -34,18 +37,24 @@ while not done:
     if event.type == pygame.QUIT:
       done = True
     #End If
+ for event in pygame.event.get():
+  if event.type == pygame.QUIT:
+   done = True
+ #End If
+ if event.type == pygame.KEYDOWN:
+  if event.key == pygame.K_UP:
+    if y_padd > 0:
+      # - write logic that happens on key press here
+      y_padd = y_padd - 5
+  elif event.key == pygame.K_DOWN:
+    if y_padd < 420:
+      y_padd = y_padd + 5
+    #End If
+  #End If
 
  #Next event
- # -- Game logic goes after this comment
- # -- Screen background is BLACK
- screen.fill (BLACK)
- # -- Draw here
- 
- pygame.draw.rect(screen, BLUE, (x_val,y_val,ball_width,ball_width))
 
- pygame.draw.rect(screen, WHITE, (x_padd,y_padd,padd_length,padd_width))
- 
- #pygame.draw.circle(screen, BLUE, (x_val,y_val),ball_width,0)
+ # -- Game logic goes after this comment
 
  x_val = x_val + xspeed
  y_val = y_val + yspeed
@@ -69,9 +78,11 @@ while not done:
 
  if top_right_x < -4:
   winsound.Beep(400,800)
-  x_val = 640/2
-  y_val = 480/2
-  xspeed = xspeed * -1
+  x_val = 320
+  y_val = 240
+  xspeed = 1
+  yspeed = 1
+  score = 0
  #End if
 
  if y_val <= 0:
@@ -89,11 +100,14 @@ while not done:
         yspeed = yspeed + 1
       else:
         yspeed = yspeed - 1
+      #End if
 
+      score = score + 1
       
       winsound.Beep(1200,40)
     #End if
   #End if
+
  elif (bottom_left_y >= y_padd):
   if (bottom_left_y <= y_padd + 60):
     if x_val <= x_padd + 15:
@@ -104,27 +118,26 @@ while not done:
         yspeed = yspeed + 1
       else:
         yspeed = yspeed - 1
+      #End if
       
+      score = score + 1
+
       winsound.Beep(1200,40)
     #End if
   #End if
  #End if
 
- for event in pygame.event.get():
-  if event.type == pygame.QUIT:
-   done = True
- #End If
- if event.type == pygame.KEYDOWN:
-  if event.key == pygame.K_UP:
-    if y_padd > 0:
-      # - write logic that happens on key press here
-      y_padd = y_padd - 5
-  elif event.key == pygame.K_DOWN:
-    if y_padd < 420:
-      y_padd = y_padd + 5
- # - write logic that happens on key press here
- #End If
- #End If
+ # -- Screen background is BLACK
+ screen.fill (BLACK)
+ # -- Draw here
+ 
+ pygame.draw.rect(screen, BLUE, (x_val,y_val,ball_width,ball_width))
+
+ pygame.draw.rect(screen, WHITE, (x_padd,y_padd,padd_length,padd_width))
+
+ score1 = font.render(str(score), True, (255, 255, 255))
+ screen.blit(score1, (500, 50))
+
  #Next event 
 
  # -- flip display to reveal new position of objects
