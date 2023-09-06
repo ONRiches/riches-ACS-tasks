4be56cef9d8442aa2750302c3e3b9f4bebde87d2
ONRiches <113181603+ONRiches@users.noreply.github.com>
2023-09-06 12:06:06 +0100

    Revert "Revert "Jetpack + Scrolling BG""
    This reverts commit ef98dd6b6feb0de8fdba0389c92d32e1481e6fc0.

--- Project/project.py
+++ Project/project.py
@@ -1,9 +1,15 @@
 import pygame
+import math
 vec = pygame.math.Vector2
 
 # -- Global Constants
-GRAVITY = 1
+GRAVITY = 8
 JUMPSPEED = -20
+
+Width = 1200
+Height = 600
+
+bg = pygame.image.load("Project/TreesBG.jpg").convert
 
 # -- Colours
 BLACK = (0, 0, 0)
@@ -23,8 +29,8 @@ class player(pygame.sprite.Sprite):
 
         # --- Set Player Position ---
         self.rect = self.image.get_rect()
-        self.rect.x = 100
-        self.rect.y = 835
+        self.rect.x = 500
+        self.rect.y = 550
 
     # --- Functions ---
 
@@ -56,16 +62,18 @@ class player(pygame.sprite.Sprite):
             return True
         
     def jumpinit(self,vertSpeed):
-        self.vel = vertSpeed
-        while player.checkCollion(self) == True:
-            player.jump(self)
+        if player.checkCollision(self) == True:
+            self.jump(vertSpeed)
 
-    def jump(self):
-        self.rect.y = self.rect.y + self.vel
-        self.vel = self.vel + GRAVITY
+    def jump(self,vertSpeed):
+            self.vel = vertSpeed
+     #   while self.checkCollision == True:
+            self.rect.y = self.rect.y + self.vel
+            self.vel = self.vel - GRAVITY
+            allspritegroup.draw
 
-    def checkCollion(self):
-        if pygame.sprite.spritecollide(myplayer,platformgroup,False) == [] or self.rect.y > 830:
+    def checkCollision(self):
+        if self.rect.colliderect(myplayer.rect):
             return True
        
 
@@ -85,16 +93,16 @@ class platform(pygame.sprite.Sprite):
         self.rect.y = posY
 
 
-# -- Initialise PyGame
+# -- Initialise PyGame --
 pygame.init()
-# -- Blank Screen
-size = (1440, 900)
+# -- Blank Screen --
+size = (Width, Height)
 screen = pygame.display.set_mode(size)
-# -- Title of new window/screen
+# -- Title of new window/screen --
 pygame.display.set_caption("My Window")
-# -- Exit game flag set to false
+# -- Exit game flag set to false --
 done = False
-# -- Manages how fast screen refreshes
+# -- Manages how fast screen refreshes --
 clock = pygame.time.Clock()
 
 allspritegroup = pygame.sprite.Group()
@@ -103,17 +111,39 @@ playergroup = pygame.sprite.Group()
 
 
 myplayer = player()
-platform1 = platform(200, 25, WHITE, 500, 500)
-platform2 = platform(100, 25, WHITE, 900, 300)
+#platform1 = platform(200, 25, WHITE, 500, 500)
+#platform2 = platform(100, 25, WHITE, 900, 300)
 
 playergroup.add(myplayer)
 allspritegroup.add(myplayer)
-platformgroup.add(platform1)
-platformgroup.add(platform2)
-allspritegroup.add(platformgroup)
+#platformgroup.add(platform1)
+#platformgroup.add(platform2)
+#allspritegroup.add(platformgroup)
 
-### -- Game Loop
+
+# -- Load image for the background, and change size to screen size --
+bg1 = pygame.image.load("Project/TreesBG.jpg").convert()
+bg = pygame.transform.scale(bg1, (Width, Height))
+bg_width = bg.get_width()
+
+scroll = 0
+tiles = math.ceil(Width / bg_width) + 1
+
+# -- Game Loop --
 while not done:
+
+
+    # -- Scroll the background image --
+    for i in range(0, tiles):
+        screen.blit(bg, (i * bg_width + scroll, 0))
+
+    scroll -= 5
+
+    if abs(scroll) > bg_width:
+        scroll = 0
+
+
+
     # -- User input and controls
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -124,23 +154,28 @@ while not done:
         allspritegroup.update()
     
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        if myplayer.rect.x < 0:
-            myplayer.rect.x = 0
-        else:
-            myplayer.rect.x = myplayer.rect.x - 15
-    if keys[pygame.K_RIGHT]:
-        if myplayer.rect.x >= 1440 - 50:
-            myplayer.rect.x = 1440 - 50
-        else:
-            myplayer.rect.x = myplayer.rect.x + 15
+    #if keys[pygame.K_LEFT]:
+        #if myplayer.rect.x < 0:
+         #   myplayer.rect.x = 0
+        #else:
+         #   myplayer.rect.x = myplayer.rect.x - 15
+    #if keys[pygame.K_RIGHT]:
+        #if myplayer.rect.x >= 1440 - 50:
+         #   myplayer.rect.x = 1440 - 50
+        #else:
+         #   myplayer.rect.x = myplayer.rect.x + 15
     if keys[pygame.K_UP]:
-        myplayer.jump(JUMPSPEED)
+        myplayer.jumpinit(JUMPSPEED)
+
+    # -- Prevent player from falling through the floor --
+    if myplayer.rect.y < 550:
+        myplayer.rect.y = myplayer.rect.y + GRAVITY
 
         
 
     # -- Screen background is BLACK
-    screen.fill(BLACK)
+   # screen.fill(BLACK)
+    pygame.display.update()
     # -- Draw here
     allspritegroup.draw(screen)
     # -- flip display to reveal new position of objects
