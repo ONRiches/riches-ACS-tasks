c198839fb0d4176c52063c4075f3c48199c1ae46
ONRiches <113181603+ONRiches@users.noreply.github.com>
2023-10-04 11:39:28 +0100

    Highscore added

--- Project/project.py
+++ Project/project.py
@@ -1,8 +1,11 @@
+from typing import Any
 import pygame
 import math
 import random
+import os
 vec = pygame.math.Vector2
 pygame.font.init()
+pygame.display.init()
 
 # -- Global Constants --
 
@@ -25,6 +28,18 @@ scroll = 0
 MaxCoins = 25
 
 font1 = pygame.font.Font('Project/Fonts/04B_30__.TTF', 30)
+font2 = pygame.font.Font('Project/Fonts/04B_30__.TTF', 15)
+
+#sprite_sheet_image = pygame.image.load('Project/Images/BarryFullSpriteSheet.png').convert_alpha()
+#sprite_sheet = spritesheet.Spritesheet(sprite_sheet_image)
+
+if os.path.exists('Project/highscore.txt'):
+    with open('Project/highscore.txt', 'r') as file:
+        high_score = file.read()
+else:
+    high_score = 6
+
+print(high_score)
 
 # -- Sprite Groups --
 
@@ -80,6 +95,21 @@ class Menu():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     print("Pause")
+
+    def deathscreen(self):
+        screen.fill(BLACK)
+
+    def checkplay(self):
+        self.deathscreen()
+        startbutton = Button(53, 153, pygame.image.load('Project/Images/start_btn.png').convert_alpha())
+        endbutton = Button(456, 153, pygame.image.load('Project/Images/exit_btn.png').convert_alpha())
+        startbutton.draw()
+        endbutton.draw()
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    print('hahaha')
+        
     
 
 
@@ -101,7 +131,6 @@ class Button():
             if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
                 self.clicked = True
                 action = True
-                print("oaengowjro")
 
         if pygame.mouse.get_pressed()[0] == 0:
             self.clicked = False
@@ -122,6 +151,7 @@ class Scoreboard():
         screen.blit(img, (x, y))
 
     def pickupcoins(self):
+        self.draw_text("score:", font2, WHITE, Width - 65, 5)
         if pygame.sprite.spritecollide(myplayer, coingroup, True):
             self.score += 1
         score = str(self.score)
@@ -132,15 +162,27 @@ class Scoreboard():
         else:
             self.draw_text(score, font1, WHITE, Width - 45, 20)
 
-    def checkplayercollision(self):
-        if pygame.sprite.groupcollide(playergroup, barriergroup, True, False):
-            pygame.quit()
-            print("oof")
+    def getscore(self):
+        return self.score
 
+    def checkplayercollision(self):
+        if pygame.sprite.groupcollide(playergroup, barriergroup, True, False, pygame.sprite.collide_mask):
+            return True
+    
     def checkcoincollision(self):
         if pygame.sprite.groupcollide(barriergroup, coingroup, False, True):
             coingroup.empty()
 
+    def draw_high_score(self, text, font, text_col, x, y):
+        self.draw_text("highscore:", font2, WHITE, 0, 5)
+        img = font.render(text, True, text_col)
+        screen.blit(img, (x, y))
+
+    def checkscore(self, score, highscore):
+        if int(score) > int(highscore):
+            highscore = score
+            with open('Project/highscore.txt', 'w') as file:
+                file.write(str(highscore))
 
 
 class player(pygame.sprite.Sprite):
@@ -157,6 +199,10 @@ class player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = Width / 3
         self.rect.y = Height - 60 - 20
+
+    #    self.frame0 = sprite_sheet.get_image()
+     #   self.frame1 = sprite_sheet.get_image()
+      #  self.frame3 = sprite_sheet.get_image()
 
     # --- Functions ---
 
@@ -181,24 +227,11 @@ class player(pygame.sprite.Sprite):
     def stop(self):
         self.playerSetSpeed(myplayer, 0)
 
-    def checkMoving(self):
-        if self.getVertSpeed(self) == 0:
-            return False
-        else:
-            return True
-
     def jump(self,vertSpeed):
+        if self.rect.y > 0:
             self.rect.y += vertSpeed
-
-    def checkCollision(self):
-        if self.rect.colliderect(myplayer.rect):
-            return True
         
-    def jumpinit(self,vertSpeed):
-        if self.checkCollision() == True:
-            self.jump(vertSpeed)
        
-
 
 class Barrier(pygame.sprite.Sprite):
     # --- Constructor Function ---
@@ -207,6 +240,9 @@ class Barrier(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         img = pygame.image.load("Project/Images/SpikyStick.png")
         self.image = pygame.transform.scale(img, (width, height))
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+        self.mask_image = self.mask.to_surface()
       #  self.image = pygame.Surface((width, height))
      #   self.image.fill(WHITE)
         self.rect = self.image.get_rect()
@@ -231,6 +267,7 @@ class Barrier(pygame.sprite.Sprite):
         self.rect.move_ip(COINSPEED * SPEEDMOD, 0)
         if self.rect.x < -75:
             self.kill()
+        
 
 
 
@@ -273,6 +310,7 @@ size = (Width, Height)
 screen = pygame.display.set_mode(size)
 # -- Variables --
 game_paused = True
+death = False
 # -- Title of new window/screen --
 pygame.display.set_caption("My Window")
 # -- Exit game flag set to false --
@@ -280,6 +318,7 @@ done = False
 # -- Manages how fast screen refreshes --
 clock = pygame.time.Clock()
 
+menu = Menu()
 
 myplayer = player()
 
@@ -298,7 +337,18 @@ allspritegroup.add(coingroup)
 # -- Game Loop --
 while not done:
 
-    if game_paused == True:
+    if death == True:
+        menu.checkplay()
+        scoreboard.draw_high_score(high_score, font1, WHITE, 45, 20)
+        scoreboard.checkscore(scoreboard.getscore(), high_score)
+        allspritegroup.update
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                done = True
+
+
+    elif game_paused == True:
 
         if startbutton.draw() == True:
             game_paused = False
@@ -316,6 +366,7 @@ while not done:
                 if event.key == pygame.K_ESCAPE:
                     print("Pause")
                     game_paused = False
+                    
     else:
         clock.tick(FPS)
 
@@ -323,7 +374,7 @@ while not done:
         bg.draw_ground()
 
         scroll += 2
-        SPEEDMOD = SPEEDMOD * scoreboard.score / 400 + 1
+        SPEEDMOD = SPEEDMOD * scoreboard.score / 200 + 1
 
         Coins.spawn()
         Barrier.spawn()
@@ -331,7 +382,7 @@ while not done:
         # -- Fly --
         keys = pygame.key.get_pressed()
         if keys[pygame.K_UP]:
-            myplayer.jumpinit(JUMPSPEED)
+            myplayer.jump(JUMPSPEED)
 
         # -- Prevent player from falling through the floor --
         if myplayer.rect.y <= Height - 62 - 20:
@@ -364,7 +415,11 @@ while not done:
 
         scoreboard.pickupcoins()
         scoreboard.checkcoincollision()
-        scoreboard.checkplayercollision()
+        if scoreboard.checkplayercollision() == True:
+            death = True
+
+        scoreboard.draw_high_score(high_score, font1, WHITE, 45, 20)
+        scoreboard.checkscore(scoreboard.getscore(), high_score)
 
     # -- flip display to reveal new position of objects
     pygame.display.flip()
